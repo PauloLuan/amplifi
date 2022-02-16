477f5cdf8499289f8d36063c82b138822704b282
Paulo Luan <pauloluan.inova@gmail.com>
2022-02-16 09:46:07 -0300

    📦 new: mapped types for comments and oposts

---
+++ packages/amplify-web-app/src/pages/interfaces.ts
@@ -0,0 +1,14 @@
+export type Post = {
+  userId: number
+  id: number
+  title: string
+  body: string
+}
+
+export type Comment = {
+  postId: number
+  id: number
+  name: string
+  email: string
+  body: string
+}
