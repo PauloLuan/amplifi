cde41ddf23294938b532e974fbe99860e35d6a9a
Paulo Luan <pauloluan.inova@gmail.com>
2022-02-16 10:43:08 -0300

    💎 style: added colors to theme

--- packages/shared-ui/src/theme.ts
+++ packages/shared-ui/src/theme.ts
@@ -14,7 +14,32 @@ export default {
     bold: 600,
     extraBold: 900
   },
-  colors: {},
+  colors: {
+    primary: '#075985',
+    secondary: '#78716C',
+    mainBg: '#F6F9FC',
+    white: '#F9FAFB',
+    black: '#030517',
+    lightGray: '#EAEAEA',
+    gray: '#8F8F8F',
+    darkGray: '#2E2F42',
+    success: '#16A34A',
+    danger: '#DC2626',
+    alert: '#EAB308',
+    info: '#0284C7',
+
+    blue: '#5429CC',
+    blueLight: '#6933FF',
+    red: '#E52E4D',
+    green: '#33CC95',
+    textTitle: '#363F5F',
+    textBody: '#969CB3',
+    background: '#F0F2F5',
+    shape: '#FFFFFF',
+
+    border: '#d7d7d7',
+    formBackground: '#e7e9ee'
+  },
   spacings: {
     xxsmall: '0.8rem',
     xsmall: '1.6rem',
