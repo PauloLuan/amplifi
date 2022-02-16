86777fafaaa4950cdb0802cbf77ab3f357e8dce9
Paulo Luan <pauloluan.inova@gmail.com>
2022-02-16 09:43:43 -0300

    ✨ feat: configured main file with browser routes and query client provider

---
+++ packages/amplify-web-app/src/main.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { QueryClientProvider } from 'react-query'
+import { BrowserRouter } from 'react-router-dom'
+import { App } from './App'
+import { queryClient } from './services/queryClient'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
