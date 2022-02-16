58c161bd3557bc7b9f223e92cd4ef281e2c5e791
Paulo Luan <pauloluan.inova@gmail.com>
2022-02-16 14:23:50 -0300

    ✅ test: fixed posts unit test

--- packages/shared-ui/tests/components/Posts/Posts.spec.tsx
+++ packages/shared-ui/tests/components/Posts/Posts.spec.tsx
@@ -3,11 +3,12 @@ import { screen } from '@testing-library/react'
 import { renderWithTheme } from '../../../src/utils'
 
 import { Posts } from '../../../src/components/Posts/Posts.component'
+import { posts } from './Posts.data'
 // import theme from '../../styles/Theme'
 
 describe('<Posts />', () => {
   it('should render the Posts component', () => {
-    renderWithTheme(<Posts />)
+    renderWithTheme(<Posts posts={posts} />)
     expect(screen.getByTestId('post-id')).toBeInTheDocument()
   })
 })
