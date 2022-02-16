92781d5d047d7618720710ef1b1e26e9ce980d7b
Paulo Luan <pauloluan.inova@gmail.com>
2022-02-16 11:51:59 -0300

    ✅ test: added a wrapper for queryclient provider in order to make tests pass

--- packages/amplify-web-app/src/pages/Home/Home.spec.tsx
+++ packages/amplify-web-app/src/pages/Home/Home.spec.tsx
@@ -1,13 +1,21 @@
-import React from 'react'
-import { screen } from '@testing-library/react'
 import { renderWithTheme } from '@pauloluan/shared'
-
+import { screen } from '@testing-library/react'
+import { QueryClient, QueryClientProvider } from 'react-query'
 import { Home } from '.'
+
 // import theme from '../Theme'
 
 describe('<Home />', () => {
+  const queryClient = new QueryClient()
+  const Wrapper = ({ children }: any) => (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  )
   it('should render the Home component and find it by Test Id', () => {
-    renderWithTheme(<Home testId="component-home-id" />)
+    renderWithTheme(
+      <Wrapper>
+        <Home testId="component-home-id" />
+      </Wrapper>
+    )
     expect(screen.getByTestId(/Home/i)).toBeDefined()
   })
 })
