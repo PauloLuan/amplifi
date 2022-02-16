346b6c7b3891c0d11b9605e533164b53f73e9122
Paulo Luan <pauloluan.inova@gmail.com>
2022-02-16 10:35:02 -0300

    📦 new: added root theme for styled components

---
+++ packages/shared-ui/src/theme.ts
@@ -0,0 +1,32 @@
+export default {
+  grid: {
+    container: '130rem',
+    gutter: '3.2rem'
+  },
+  border: {
+    radius: '0.4rem'
+  },
+  font: {
+    family: 'Roboto',
+    extraLight: 100,
+    light: 300,
+    normal: 400,
+    bold: 600,
+    extraBold: 900
+  },
+  colors: {},
+  spacings: {
+    xxsmall: '0.8rem',
+    xsmall: '1.6rem',
+    small: '2.4rem',
+    medium: '3.2rem',
+    large: '4.0rem',
+    xlarge: '4.8rem',
+    xxlarge: '5.6rem'
+  },
+  layers: {},
+  transition: {
+    default: '0.3s ease-in-out',
+    fast: '0.1s ease-in-out'
+  }
+} as const
